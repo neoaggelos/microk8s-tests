59846d22918709fb889896d1805b5e8eff46b6ca
Angelos Kolaitis <neoaggelos@gmail.com>
2023-08-24 13:11:32 +0300

    cleanup ceph tests, add cephfs

--- ceph/microceph.sh
+++ ceph/microceph.sh
@@ -21,10 +21,14 @@ fi
 
 export JUJU_MODEL="${JUJU_MODEL}"
 
+# Install Ceph
 juju run --machine 0 -- '
 sudo snap install microceph --channel '"${MICROCEPH_CHANNEL}"'
 sudo microceph cluster bootstrap
+'
 
+# Create OSDs
+juju run --machine 0 -- '
 for l in a b c; do
   loop_file="$(sudo mktemp -p /mnt XXXX.img)"
   sudo truncate -s 1G "${loop_file}"
@@ -33,6 +37,11 @@ for l in a b c; do
   sudo mknod -m 0660 "/dev/sdi${l}" b 7 "${minor}"
   sudo microceph disk add --wipe "/dev/sdi${l}"
 done
+'
+
+# Create CephFS
+juju run --machine 0 -- '
+sudo ceph fs volume create fs0
 '
 
 # Install MicroK8s
@@ -49,25 +58,44 @@ sudo microk8s enable rook-ceph
 # Connect with microceph
 juju run --machine 0 "sudo microk8s connect-external-ceph"
 
-# deploy example pods with 1, 2 and 3 replicas
+# deploy example pods with cephfs and ceph-rbd
 juju run --machine 0 'sudo microk8s.kubectl apply -f - <<EOF
 ---
 apiVersion: v1
 kind: PersistentVolumeClaim
-metadata: { name: pvc }
+metadata: { name: pvc-rbd }
 spec: { storageClassName: ceph-rbd, accessModes: [ReadWriteOnce], resources: { requests: { storage: 500Mi } } }
 ---
 apiVersion: v1
 kind: Pod
-metadata: { name: nginx }
+metadata: { name: nginx-rbd }
 spec:
-  volumes: [{ name: pvc, persistentVolumeClaim: { claimName: pvc } }]
+  volumes: [{ name: pvc, persistentVolumeClaim: { claimName: pvc-rbd } }]
+  containers: [{ name: nginx, image: nginx, volumeMounts: [{ name: pvc, mountPath: /usr/share/nginx/html }] }]
+---
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata: { name: pvc-fs }
+spec: { storageClassName: cephfs, accessModes: [ReadWriteMany], resources: { requests: { storage: 500Mi } } }
+---
+apiVersion: v1
+kind: Pod
+metadata: { name: nginx-fs-1 }
+spec:
+  volumes: [{ name: pvc, persistentVolumeClaim: { claimName: pvc-fs } }]
+  containers: [{ name: nginx, image: nginx, volumeMounts: [{ name: pvc, mountPath: /usr/share/nginx/html }] }]
+---
+apiVersion: v1
+kind: Pod
+metadata: { name: nginx-fs-2 }
+spec:
+  volumes: [{ name: pvc, persistentVolumeClaim: { claimName: pvc-fs } }]
   containers: [{ name: nginx, image: nginx, volumeMounts: [{ name: pvc, mountPath: /usr/share/nginx/html }] }]
 EOF
 '
 
 # wait for volumes and pvcs
-while ! juju run --machine 0 "sudo microk8s.kubectl wait --for=condition=ready pod/nginx"
+while ! juju run --machine 0 "sudo microk8s.kubectl wait --for=condition=ready pod/nginx-rbd pod/nginx-fs-1 pod/nginx-fs-2"
 do
   echo 'waiting for pods to come online'
   sleep 5
